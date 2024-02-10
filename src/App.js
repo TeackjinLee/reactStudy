f1da47ab6b23bed991e2caf21f7f71c716d2fafc
이택진 <85720454+TeackjinLee@users.noreply.github.com>
2024-02-10 22:50:23 +0900

    2.4.3 if 문 대신 조건무 연산자
    2.4.3 if 문 대신 조건무 연산자

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
 // import React, {Fragment} from 'react';
 import React from 'react';
 
+// function App() {
+//   const name = '리액트';
+//   return (
+//     // <div>
+//     //   Hello <b>react</b>
+//     // </div>
+//     // <div>
+//     //   <h1>리액트 안녕!</h1>
+//     //   <h1>리액트 안녕!</h1>
+//     // </div>
+    
+//     <>
+//       <h1>{name} 안녕!</h1>
+//       <h2>잘 작동하니?</h2>
+//     </>
+//   )
+// }
+
+//2.4.3 if 문 대신 조건무 연산자
 function App() {
   const name = '리액트';
   return (
-    // <div>
-    //   Hello <b>react</b>
-    // </div>
-    // <div>
-    //   <h1>리액트 안녕!</h1>
-    //   <h1>리액트 안녕!</h1>
-    // </div>
-    
-    <>
-      <h1>{name} 안녕!</h1>
-      <h2>잘 작동하니?</h2>
-    </>
+    <div>
+      {name === '리액트' ? (
+        <h1>리액트 입니다.</h1>
+      ) : (
+        <h2>리액트가 아닙니다.</h2>
+      )}
+    </div>
   )
 }
 
