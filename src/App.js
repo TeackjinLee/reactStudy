0978110bb1f95b689cc731f90dbaabdec4965d37
이택진 <85720454+TeackjinLee@users.noreply.github.com>
2024-02-10 22:41:10 +0900

    JSX 감싸인 요소
    JSX 감싸인 요소

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
+// import React, {Fragment} from 'react';
 import React from 'react';
 
 function App() {
+  const name = '리액트';
   return (
     // <div>
     //   Hello <b>react</b>
     // </div>
-    <div>
-      <h1>리액트 안녕!</h1>
-      <h1>리액트 안녕!</h1>
-    </div>
+    // <div>
+    //   <h1>리액트 안녕!</h1>
+    //   <h1>리액트 안녕!</h1>
+    // </div>
+    
+    <>
+      <h1>{name} 안녕!</h1>
+      <h2>잘 작동하니?</h2>
+    </>
   )
 }
 
