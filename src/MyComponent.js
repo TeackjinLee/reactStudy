fc7a9444cea30848b93d0f5ddc0e066c1bfeff98
이택진 <85720454+TeackjinLee@users.noreply.github.com>
2024-02-12 00:05:58 +0900

    3.3.5 비구조화 할당 문법을 통해 props 내부 값 추출하기
    3.3.5 비구조화 할당 문법을 통해 props 내부 값 추출하기

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -18,11 +18,23 @@ import React from "react";
 //   name: "기본 이름",
 // };
 
+// const MyComponent = (props) => {
+//   return (
+//     <div>
+//       안녕하세요, 제이름은 {props.name}입니다. <br />
+//       children 값은 {props.children}
+//       입니다.
+//     </div>
+//   );
+// };
+
+// 3.3.5 비구조화 할당 문법을 통해 props 내부 값 추출하기
 const MyComponent = (props) => {
+  const { name, children } = props;
   return (
     <div>
-      안녕하세요, 제이름은 {props.name}입니다. <br />
-      children 값은 {props.children}
+      안녕하세요, 제 이름은 {name}입니다. <br />
+      children 값은 {children}
       입니다.
     </div>
   );
