19aef72bb9d594b66d25942186f985a54579cfdd
이택진 <85720454+TeackjinLee@users.noreply.github.com>
2024-02-11 17:27:35 +0900

    2.4.5 undefined를 랜더링하지 않기
    2.4.5 undefined를 랜더링하지 않기

--- src/App.js
+++ src/App.js
@@ -34,12 +34,19 @@ import React from 'react';
 // }
 
 // 2.4.4 AND 연산자(&&)를 사용한 조건부 렌더링
+// function App() {
+//   const name = '뤼왝트';
+
+//   // return <div>{name === '리액트' ? <h1>리액트 입니다.</h1> : null }</div>
+//   return <div>{name === '리액트' && <h1>리액트 입니다.</h1>}</div>
+
+// }
+
+// 2.4.5 undefined를 랜더링하지 않기
 function App() {
-  const name = '뤼왝트';
-
-  // return <div>{name === '리액트' ? <h1>리액트 입니다.</h1> : null }</div>
-  return <div>{name === '리액트' && <h1>리액트 입니다.</h1>}</div>
-
+  const name = undefined;
+  // return name || '값이 undefined입니다.';
+  return <div>{name || '리액트'}</div>;
 }
 
 export default App;
