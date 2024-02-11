cd2b278485e65ad99842aad29e8ce09be7d4cd26
이택진 <85720454+TeackjinLee@users.noreply.github.com>
2024-02-11 17:40:50 +0900

    2.4.6 인라인 스타일링
    2.4.6 인라인 스타일링

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // import React, {Fragment} from 'react';
 import React from 'react';
+import './App.css'
 
 // function App() {
 //   const name = '리액트';
@@ -53,16 +54,17 @@ import React from 'react';
 function App() {
   const name = '리액트';
   return (
-    <div
-      style = {{
-        // background-color는 backgroundColor와 같이 -가 사라지고 카멘 표기법으로 작성됩니다.
-        backgroundColor : 'black',
-        color : 'aqua',
-        fontSize : '48px', // font-size -> fontSize
-        fontWeight : 'bold', // font-weight -> fontWeight
-        padding : 16 // 단위를 생략하면 px로 지정됩니다.
-      }}
-    >
+    // <div
+    //   style = {{
+    //     // background-color는 backgroundColor와 같이 -가 사라지고 카멘 표기법으로 작성됩니다.
+    //     backgroundColor : 'black',
+    //     color : 'aqua',
+    //     fontSize : '48px', // font-size -> fontSize
+    //     fontWeight : 'bold', // font-weight -> fontWeight
+    //     padding : 16 // 단위를 생략하면 px로 지정됩니다.
+    //   }}
+    // >
+    <div className='react'>
       {name}
     </div>
   );
