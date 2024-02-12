49b436a513b3910414aa4fee7d0732cc78e1cf21
이택진 <85720454+TeackjinLee@users.noreply.github.com>
2024-02-12 20:42:06 +0900

    3.4.1.4 this.setState가 끝난 후 특정 작업 실행하기
    3.4.1.4 this.setState가 끝난 후 특정 작업 실행하기

--- src/Counter.js
+++ src/Counter.js
@@ -30,16 +30,33 @@ class Counter extends Component {
             // this.setState({ number: number + 1 });
             // 3.4.1.3 this.setState에 객체 대신 함수 인자 전달하기
             // this.setState({ number: this.state.number + 1 });
-            this.setState((prevState) => {
-              return {
-                number: prevState.number + 1,
-              };
-            });
+            // this.setState((prevState) => {
+            //   return (
+            //     {
+            //       number: prevState.number + 1,
+            //     },
+            //     () => {
+            //       console.log("방금 setState가 호출되었습니다.");
+            //       console.log(this.state);
+            //     }
+            //   );
+            // });
             // 위 코드와 아래 코드는 완전히 똑같은 기능을 합니다.
             // 아래 코드는 함수에서 바로 객체를 반환한다는 의미입니다.
-            this.setState((prevState) => ({
-              number: prevState.number + 1,
-            }));
+            // this.setState((prevState) => ({
+            //   number: prevState.number + 1,
+            // }));
+
+            // 3.4.1.4 this.setState가 끝난 후 특정 작업 실행하기
+            this.setState(
+              {
+                number: number + 1,
+              },
+              () => {
+                console.log("방금 setState가 호출되었습니다.");
+                console.log(this.state);
+              }
+            );
           }}
         >
           +1
