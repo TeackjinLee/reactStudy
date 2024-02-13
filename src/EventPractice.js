d40ca05ea9db4516ca46389bc905087a07a124c2
이택진 <85720454+TeackjinLee@users.noreply.github.com>
2024-02-13 23:23:24 +0900

    4.3 함수형 컴포넌트로 구현해 보기
    4.3 함수형 컴포넌트로 구현해 보기

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -1,95 +1,60 @@
-import React, { Component } from "react";
-// 4.2.1.1 컴포넌트 생성
-class EventPractice extends Component {
-  state = {
-    message: "",
-    // 4.2.4 input 여러개 다루기
+import React, { useState } from "react";
+// 4.3 함수형 컴포넌트로 구현해 보기
+const EventPractice = () => {
+  const [form, setForm] = useState({
     username: "",
+    message: "",
+  });
+  const { username, message } = form;
+  const onChange = (e) => {
+    const nextForm = {
+      ...form, // 기존의 form 내용을 이 자리에 복사한 뒤
+      [e.target.name]: e.target.value, // 원하는 값을 덮어 씌우기
+    };
+    setForm(nextForm);
   };
 
-  // 4.2.3 임의 메서드 만들기
-  // 4.2.3.1 기본방식
-  // constructor(props) {
-  //   super(props);
-  //   this.handleChange = this.handleChange.bind(this);
-  //   this.handleClick = this.handleClick.bind(this);
-  // }
-
-  // handleChange(e) {
-  //   this.setState({
-  //     message: e.target.value,
-  //   });
-  // }
-
-  // handleClick() {
-  //   alert(this.state.message);
-  //   this.setState({
-  //     message: "",
-  //   });
-  // }
-
-  // 4.2.3.2 Property Intializer Syntax를 사용한 메서드 작성
-  handleChange = (e) => {
-    this.setState({
-      // message: e.target.value,
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  handleClick = () => {
-    alert(this.state.username + ": " + this.state.message);
-    this.setState({
+  // const [username, setUsername] = useState("");
+  // const [message, setMessage] = useState("");
+  // const onChangeUsername = (e) => setUsername(e.target.value);
+  // const onChangeMessage = (e) => setMessage(e.target.value);
+  const onClick = () => {
+    alert(username + ": " + message);
+    // setUsername("");
+    // setMessage("");
+    setForm({
       username: "",
       message: "",
     });
   };
-
-  // 4.2.5 onKeyPress 이벤트 핸들링
-  handleKeyPress = (e) => {
+  const onKeyPress = (e) => {
     if (e.key === "Enter") {
-      this.handleClick();
+      onClick();
     }
   };
-
-  render() {
-    return (
-      <div>
-        <h1>이벤트 연습</h1>
-        <input
-          type="text"
-          name="username"
-          placeholder="사용자명"
-          value={this.state.username}
-          onChange={this.handleChange}
-        />
-        <input
-          type="text"
-          name="message"
-          placeholder="아무거나 입력해 보세요"
-          value={this.state.message}
-          //   onChange={(e) => {
-          //     this.setState({
-          //       message: e.target.value,
-          //     });
-          //   }}
-          onChange={this.handleChange}
-          onKeyPress={this.handleKeyPress}
-        />
-        {/* 4.2.2.3 버튼을 누를 때 comment 값을 공백으로 설정 */}
-        {/* <button
-          onClick={() => {
-            alert(this.state.message);
-            this.setState({
-              message: "",
-            });
-          }}
-        >
-          확인
-        </button> */}
-        <button onClick={this.handleClick}>확인</button>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <h1>이벤트 연습</h1>
+      <input
+        type="text"
+        name="username"
+        placeholder="사용자명"
+        value={username}
+        // onChange={onChangeUsername}
+        onChange={onChange}
+      />
+      <input
+        type="text"
+        name="message"
+        placeholder="아무거나 입력해 보세요"
+        value={message}
+        // onChange={onChangeMessage}
+        onChange={onChange}
+        onKeyPress={onKeyPress}
+      />
+      <button onClick={onClick}>확인</button>
+    </div>
+  );
+};
 
 export default EventPractice;
---
+++ src/EventPracticeClass.js
@@ -0,0 +1,95 @@
+import React, { Component } from "react";
+// 4.2.1.1 컴포넌트 생성
+class EventPractice extends Component {
+  state = {
+    message: "",
+    // 4.2.4 input 여러개 다루기
+    username: "",
+  };
+
+  // 4.2.3 임의 메서드 만들기
+  // 4.2.3.1 기본방식
+  // constructor(props) {
+  //   super(props);
+  //   this.handleChange = this.handleChange.bind(this);
+  //   this.handleClick = this.handleClick.bind(this);
+  // }
+
+  // handleChange(e) {
+  //   this.setState({
+  //     message: e.target.value,
+  //   });
+  // }
+
+  // handleClick() {
+  //   alert(this.state.message);
+  //   this.setState({
+  //     message: "",
+  //   });
+  // }
+
+  // 4.2.3.2 Property Intializer Syntax를 사용한 메서드 작성
+  handleChange = (e) => {
+    this.setState({
+      // message: e.target.value,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  handleClick = () => {
+    alert(this.state.username + ": " + this.state.message);
+    this.setState({
+      username: "",
+      message: "",
+    });
+  };
+
+  // 4.2.5 onKeyPress 이벤트 핸들링
+  handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      this.handleClick();
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>이벤트 연습</h1>
+        <input
+          type="text"
+          name="username"
+          placeholder="사용자명"
+          value={this.state.username}
+          onChange={this.handleChange}
+        />
+        <input
+          type="text"
+          name="message"
+          placeholder="아무거나 입력해 보세요"
+          value={this.state.message}
+          //   onChange={(e) => {
+          //     this.setState({
+          //       message: e.target.value,
+          //     });
+          //   }}
+          onChange={this.handleChange}
+          onKeyPress={this.handleKeyPress}
+        />
+        {/* 4.2.2.3 버튼을 누를 때 comment 값을 공백으로 설정 */}
+        {/* <button
+          onClick={() => {
+            alert(this.state.message);
+            this.setState({
+              message: "",
+            });
+          }}
+        >
+          확인
+        </button> */}
+        <button onClick={this.handleClick}>확인</button>
+      </div>
+    );
+  }
+}
+
+export default EventPractice;
