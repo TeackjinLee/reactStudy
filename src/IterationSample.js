56b47ddf4e986b381d6a5044d4ff93577216de37
이택진 <85720454+TeackjinLee@users.noreply.github.com>
2024-02-15 22:16:29 +0900

    6.3.1 key 설정
    6.3.1 key 설정

--- src/IterationSample.js
+++ src/IterationSample.js
@@ -3,7 +3,8 @@ import React from 'react';
 // 6.2.1 컴포넌트 수정하기
 const IterationSample = () => {
     const names = ["눈사람", "얼음", "눈", "바람"];
-    const nameList = names.map(name => <li>{name}</li>);
+    // 6.3.1 key 설정
+    const nameList = names.map((name, index) => <li key={index}>{name}</li>);
     return <ul>{nameList}</ul>
 }
 
